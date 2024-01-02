725d2cc7435dc3d071028d11ed3227906146acac
atsuatsupoteto <atsuatsu.poteto@gmail.com>
2024-01-02 12:02:03 +0900

    feat:環境構築後最初のコミット

---
+++ resources/ts/app.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import { TopPage } from "./components/TopPage";
+import { MyPage } from "./components/MyPage";
+import { PostPage } from "./components/PostPage";
+
+const App = () => {
+    const title: string = "Hello TypeScript React";
+    return (
+        <div id="main">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<TopPage />} />
+                    <Route path="/mypage" element={<MyPage />} />
+                    <Route path="/posts" element={<PostPage />} />
+                </Routes>
+                <ul>
+                    <li>
+                        <Link to="/">Top</Link>
+                    </li>
+                    <li>
+                        <Link to="/mypage">My</Link>
+                    </li>
+                    <li>
+                        <Link to="/posts">Post</Link>
+                    </li>
+                </ul>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+const container = document.getElementById("app") as HTMLInputElement;
+const root = createRoot(container);
+root.render(<App />);
